ded7645a33547d60edd855db3591be50cf6931fc
sanskaraggarwal2025 <aggarwalsanskar992@gmail.com>
2023-08-01 09:16:02 +0530

    Cart page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import CartPage from './pages/CartPage';
 
 import {
   createBrowserRouter,
@@ -22,6 +23,10 @@ const router = createBrowserRouter([
     path: '/signup',
     element: <SignupPage></SignupPage>,
   },
+  {
+    path: '/cart',
+    element: <CartPage></CartPage>
+  }
 ]);
 
 function App() {
