fda709a00fc25ad2ff9ba40cb0039dab44f76b99
sanskaraggarwal2025 <aggarwalsanskar992@gmail.com>
2023-08-02 09:41:50 +0530

    checkout page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,13 @@ import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import CartPage from './pages/CartPage';
+import Checkout from './pages/Checkout';
 
 import {
   createBrowserRouter,
   RouterProvider,
 } from 'react-router-dom';
+import ProductDetailPage from './pages/ProductDetailPage';
 
 const router = createBrowserRouter([
   {
@@ -26,7 +28,16 @@ const router = createBrowserRouter([
   {
     path: '/cart',
     element: <CartPage></CartPage>
+  },
+  {
+    path: '/checkout',
+    element: <Checkout></Checkout>
+  },
+  {
+    path: '/product-detail',
+    element: <ProductDetailPage></ProductDetailPage>
   }
+
 ]);
 
 function App() {
